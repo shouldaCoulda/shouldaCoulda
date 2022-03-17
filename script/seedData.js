0410c317fe6265c6d9efad1517e11bd36d991562
Dale Luce <daleluce87@gmail.com>
2022-03-17 17:55:32 -0400

    script and seed data nearly finished.

---
+++ script/seedData.js
@@ -0,0 +1,108 @@
+export const subscriptionData = [
+  {
+    name: "Netflix",
+    price: "15.49",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl:
+      "https://www.netflix.com/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fcancelplan",
+  },
+  {
+    name: "Spotify",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://support.spotify.com/us/article/cancel-premium/",
+  },
+  {
+    name: "Youtube Premium",
+    price: "11.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://www.youtube.com/paid_memberships?ybp=mAEK",
+  },
+  {
+    name: "Hulu",
+    price: "12.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Apple Music",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Tidal",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "HBO Max",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Peacock",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Disney+",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Amazon Prime",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Sling TV",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Apple TV",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Paramount+",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "ESPN+",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+  {
+    name: "Curiosity Stream",
+    price: "9.99",
+    imageUrl: "https://source.unsplash.com/random",
+    websiteUrl: "https://help.hulu.com/s/article/manage-subscription",
+  },
+];
+
+export const userDate = [
+    {
+        email: 'user@gmail.com', 
+        password: '123', 
+        isAdmin: false,
+    }
+     {
+        email: 'admin@gmail.com', 
+        password: '123', 
+        isAdmin: true,
+    }
+]
+
+
---
+++ script/seedScript.js
@@ -0,0 +1,83 @@
+// 'use strict'
+
+// const {db } = require('../server/db')
+const { fireBase } = require("../client/firebase");
+const { userDate, seedDate } = require("./seedData.js");
+
+const seeding = function () {
+  let index = 0;
+  const interval = setInterval(async () => {
+    const user = newSeedData[index];
+    const { email, password, isAdmin } = user;
+    console.log("Current user in setInterval: ", user.email);
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(async (response) => {
+        console.log("Created user in Authentication");
+        const uid = response.user.uid;
+        const data = {
+          id: uid,
+          email,
+          firstName,
+          isAdmin,
+        };
+        const usersRef = firebase.firestore().collection("users").doc(uid);
+        await usersRef.set(data);
+        console.log("Adding user to the users collection: ", data.email);
+      })
+      .catch((error) => console.log(error));
+    await firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("Sign out successful!"))
+      .catch((error) => console.log(error));
+    index += 1;
+    if (index === newSeedData.length) clearInterval(interval);
+  }, 2000);
+};
+
+seeding();
+
+// ORIGINAL CODE BELOW-----------------------------------------------------------
+// /**
+//  * seed - this function clears the database, updates tables to
+//  *      match the models, and populates the database.
+//  */
+// async function seed() {
+//   await db.sync({ force: true }) // clears db and matches models to tables
+//   console.log('db synced!')
+//   console.log(`seeded successfully`)
+
+// }
+
+// /*
+//  We've separated the `seed` function from the `runSeed` function.
+//  This way we can isolate the error handling and exit trapping.
+//  The `seed` function is concerned only with modifying the database.
+// */
+// async function runSeed() {
+//   console.log('seeding...')
+//   try {
+//     await seed()
+//   } catch (err) {
+//     console.error(err)
+//     process.exitCode = 1
+//   } finally {
+//     console.log('closing db connection')
+//     await db.close()
+//     console.log('db connection closed')
+//   }
+// }
+
+// /*
+//   Execute the `seed` function, IF we ran this module directly (`node seed`).
+//   `Async` functions always return a promise, so we can use `catch` to handle
+//   any errors that might occur inside of `seed`.
+// */
+// if (module === require.main) {
+//   runSeed()
+// }
+
+// // we export the seed function for testing purposes (see `./seed.spec.js`)
+// module.exports = seed
