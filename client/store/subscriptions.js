319da25b3e1bb31f62a019581a68f70ad06e80f4
Chao Zhou <Czhou0k@gmail.com>
2022-03-17 16:55:37 -0400

    added allsubscription action creator in store/subscriptions

--- client/store/subscriptions.js
+++ client/store/subscriptions.js
@@ -3,6 +3,13 @@ import history from '../history';
 
 const GET_ALLSUBSCRIPTIONS = 'GET_ALLSUBSCRIPTIONS';
 
+const getSubscriptions = (subscriptions) => {
+  return {
+    type: GET_ALLSUBSCRIPTIONS,
+    subscriptions,
+  };
+};
+
 const initialState = [];
 
 export default function (subscriptions = initialState, action) {
