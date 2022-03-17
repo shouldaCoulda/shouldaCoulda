96e3d5911836bf0e345254b261a92af32f55e224
joshua constine <joshua.constine97@gmail.com>
2022-03-17 13:43:08 -0500

    removed test files, and auth form

---
+++ client/components/SignUp.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+/**
+ * COMPONENT
+ */
+const SignUp = () => {
+
+  function handleSubmit() {
+    console.log('submit')
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={}>
+        <div>
+          <label htmlFor="username">
+            <small>Username</small>
+          </label>
+          <input name="username" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+export default SignUp
