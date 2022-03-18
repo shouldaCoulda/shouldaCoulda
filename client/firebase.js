bf8f575376145f0b600da9994b5649654da96109
joshua constine <joshua.constine97@gmail.com>
2022-03-18 15:23:40 -0500

    working on subscription provider

--- client/firebase.js
+++ client/firebase.js
@@ -27,4 +27,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase();
+
 // const database = getDatabase(app);
