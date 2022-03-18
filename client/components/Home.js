0727d3d53b2019f5b1d2801dec9486537e6fc871
Rob Watkins <rwatkins123@gmail.com>
2022-03-18 11:47:24 -0500

    testing firebase hookup

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 /**
  * COMPONENT
  */
 export const Home = () => {
   const { currentUser } = useAuth();
+  const database = getDatabase();
+  const name = ref(database, '/Netflix')
 
+  console.log("database checking...", database)
+
+  console.log("name check..", name)
   return (
     <div>
       <h3>Welcome to shoulda </h3>
       <p>Email:</p> {currentUser?.email}
+      {/* <h1>{name}</h1> */}
     </div>
   );
 };
