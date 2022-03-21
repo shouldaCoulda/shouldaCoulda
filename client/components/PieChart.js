b3758d5c9c3797ef73ef1da5c9e7fe1e628c8a40
Chao Zhou <Czhou0k@gmail.com>
2022-03-21 16:08:17 -0400

    half working functionatlity to pie graph

--- client/components/PieChart.js
+++ client/components/PieChart.js
@@ -2,17 +2,45 @@ import React from 'react';
 import { VictoryPie } from 'victory';
 
 const data = [
-  { x: 'Netflix', y: 130 },
-  { x: 'Hulu', y: 165 },
-  { x: 'Spotify', y: 140 },
-  { x: 'Youtube TV', y: 190 },
+  { x: 1, y: 130, label: 'Netflix' },
+  { x: 2, y: 165, label: 'Hulu' },
+  { x: 3, y: 140, label: 'Spotify' },
+  { x: 4, y: 190, label: 'Youtube TV' },
 ];
 
 const PieChart = () => {
   return (
     <div>
       Hello
-      <VictoryPie data={data} />
+      <VictoryPie
+        events={[
+          {
+            target: 'data',
+            eventHandlers: {
+              onClick: () => {
+                return [
+                  {
+                    target: 'data',
+                    mutation: ({ style }) => {
+                      return style.fill === '#c43a31' ? null : (
+                        <div>{data[0]}</div>
+                      );
+                    },
+                  },
+                  {
+                    target: 'labels',
+                    mutation: ({ text }) => {
+                      return text === 'clicked' ? null : { text: `${data}` };
+                    },
+                  },
+                ];
+              },
+            },
+          },
+        ]}
+        data={data}
+        // colorScale={['red', 'green', '#1DB954', 'cyan']}
+      />
     </div>
   );
 };
