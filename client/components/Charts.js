01a8f8d5975a8207c4ee0044e83d29a83872f0a8
Chao Zhou <Czhou0k@gmail.com>
2022-03-21 12:32:30 -0400

    updated chart

--- client/components/Charts.js
+++ client/components/Charts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { VictoryBar, VictoryChart } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 
 const data = [
   { quarter: 1, earnings: 130 },
@@ -13,7 +13,11 @@ const Charts = () => {
   return (
     <div>
       Chart
-      <VictoryChart>
+      <VictoryChart domainPadding={20}>
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={['Netflix', 'Hulu', 'Spotify', 'YoutubeTV']}
+        />
         <VictoryBar data={data} x='quarter' y='earnings' />
       </VictoryChart>
     </div>
