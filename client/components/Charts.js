ac9ded2e8ea8cc03e1956bd51fef9b130e8ec6f6
Chao Zhou <Czhou0k@gmail.com>
2022-03-21 12:20:45 -0400

    install victory.js and added dummy data

--- client/components/Charts.js
+++ client/components/Charts.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import * as V from 'victory';
+
+const data = [
+  { quarter: 1, earnings: 130 },
+  { quarter: 2, earnings: 165 },
+  { quarter: 3, earnings: 140 },
+  { quarter: 4, earnings: 190 },
+];
 
 const Charts = () => {
   return <div>Chart</div>;
