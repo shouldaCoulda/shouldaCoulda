ee3bdb4dffc64c88c1095ab3c7578c6418a5c32e
joshua constine <joshua.constine97@gmail.com>
2022-03-22 13:04:31 -0500

    axis labels updated

--- client/components/LineChart.js
+++ client/components/LineChart.js
@@ -13,23 +13,25 @@ import { useAuth } from "../contexts/AuthContext";
 const LineChart = () => {
   const { currentUser, usersSubscriptions, removeSubscription, getTotal } =
     useAuth();
-  let months = 12;
+  let months = 60;
 
   function getData(total, months) {
     const data = [];
     for (let i = 0; i < months; i++) {
-      data[i] = { x: i, y: getTotal() * i, label: Math.floor(getTotal() * i) };
+      data[i] = { x: i, y: getTotal() * i };
     }
 
-    console.log(data);
     return data;
   }
+
   const data = getData(getTotal(), months);
+  const maxY = data[data.length - 1].y * 1.2;
+  console.log(maxY);
 
   return (
     <div>
       <h1>Subscriptions: {getTotal()}</h1>
-      <VictoryChart domain={{ x: [0, 12], y: [0, 1000] }}>
+      <VictoryChart domain={{ x: [0, months], y: [0, maxY] }}>
         <VictoryLine
           data={data}
           style={{
