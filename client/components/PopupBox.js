f8cf6e4d6443ff6da81e96e30c15a7e500f7332f
Chao Zhou <Czhou0k@gmail.com>
2022-03-24 16:17:40 -0400

    added a popup box in profile page

--- client/components/PopupBox.js
+++ client/components/PopupBox.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const PopupBox = (props) => {
   return (
-    <div className='popup-box'>
+    <div className='popup'>
       <div className='box'>
-        <span className='close-icon' onClick={props.handleClose}>
+        <span className='close' onClick={props.handleClose}>
           x
         </span>
         {props.content}
