93785e64b670652516759bee2748bbe77f12ead8
Chao Zhou <Czhou0k@gmail.com>
2022-03-26 19:37:29 -0400

    added a PromptButton component

---
+++ client/components/PromptButton.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import SignInPrompt from './SignInPrompt';
+
+const PromptButton = () => {
+  return <div>Hello</div>;
+};
+
+export default PromptButton;
---
+++ client/components/SignInPrompt.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignInPrompt = () => {
+  return <div>Hello</div>;
+};
+
+export default SignInPrompt;
