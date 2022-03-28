8bf66cae8f4d40c2a77364f960757bc27c18bbf7
Chao Zhou <Czhou0k@gmail.com>
2022-03-28 12:06:42 -0400

    added more config to webpack for css module

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,21 @@
 module.exports = {
-  entry: [
-    './client/index.js'
-  ],
+  entry: ['./client/index.js'],
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename: './public/bundle.js',
   },
   devtool: 'source-map',
   module: {
     rules: [
       {
-        test: [/\.jsx?$/,/\.css$/i]
+        test: [/\.jsx?$/, /\.css$/i],
         exclude: /node_modules/,
-        loader: ['babel-loader',"style-loader", "css-loader" ]
+        loader: ['babel-loader', 'style-loader', 'css-loader'],
         options: {
-          presets: [
-            '@babel/preset-react'
-          ]
-        }
-      }
-    ]
-  }
-}
+          modules: true,
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
