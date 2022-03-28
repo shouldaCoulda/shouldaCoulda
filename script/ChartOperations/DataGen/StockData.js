51455f1c6e9e1a00560d4f6bbc9deeb2b313d62f
joshua constine <joshua.constine97@gmail.com>
2022-03-28 13:15:44 -0500

    btc rate working

--- script/ChartOperations/DataGen/StockData.js
+++ script/ChartOperations/DataGen/StockData.js
@@ -10,22 +10,23 @@ export function getStockData(total, months, btcData) {
   for (let i = 0; i < months; i++) {
     //this gets the rate of change for the compound function
     // const rateOfChange = Math.random() / 10 + 1;
-    let rateOfChange = 1
+    let rateOfChange = 1;
     if (btcData && btcData.length > months) {
       rateOfChange = getROC(i, btcData, months);
     }
-    
+
     if (i === 0) {
       data[i] = { x: i, y: 0 };
     } else if (i === 1) {
       prevtotal = 0;
       data[i] = { x: i, y: compound(prevtotal, rateOfChange, total) };
-      prevtotal = compound(prevtotal, 1.007, total);
+      prevtotal = compound(prevtotal, rateOfChange, total);
     } else {
       data[i] = { x: i, y: compound(prevtotal, rateOfChange, total) };
       prevtotal = compound(prevtotal, rateOfChange, total);
     }
   }
+  console.log(data);
   return data;
 }
 
@@ -36,9 +37,8 @@ export function compound(prev, rate, total) {
 }
 
 function getROC(i, data, months) {
-  console.log(i);
-  // if (i < months) {
-  //   console.log(data[months - i - 1] / data[months - i - 1]);
-  // }
-  return 1.07;
+  if (i < months) {
+    return Number(data[months - i - 1].price / data[months - i].price);
+  }
+  return 1;
 }
