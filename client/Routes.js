3d577f00cfd92f5b23f3ee923df2081efccdbc26
joshua constine <joshua.constine97@gmail.com>
2022-03-28 15:41:33 -0500

    updated user flow

--- client/Routes.js
+++ client/Routes.js
@@ -11,6 +11,7 @@ import SelectionScreen from "./components/SelectionScreen";
 import { SubscriptionTier } from "./components/SubscriptionTier";
 import SignInPrompt from "./components/SignInPrompt";
 import Home from "./components/Home";
+import AddExpensees from "./components/AddExpenses";
 
 const Routes = () => {
   const { currentUser } = useAuth();
@@ -24,6 +25,7 @@ const Routes = () => {
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/selections" component={SelectionScreen} />
             <Route exact path="/add" component={AddSubscription} />
+            <Route exact path="/expense" component={AddExpensees} />
             <Route exact path="/" component={ChartContainer} />
 
             <Route
--- client/components/Chart/AddButton.js
+++ client/components/Chart/AddButton.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PopupButton from "../PopupButton";
 const AddButton = () => {
   return (
-    <Link to="/home">
-      <button className="moreSubsButton hover">+</button>
-    </Link>
+    <div>
+      {/* <button className="moreSubsButton hover">+</button> */}
+      <PopupButton />
+    </div>
   );
 };
 
