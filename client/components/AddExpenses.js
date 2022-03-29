82b842870ea8efeeb3721835b3dcd089d32d8248
Rob Watkins <rwatkins123@gmail.com>
2022-03-29 12:36:16 -0500

    cable card

--- client/components/AddExpenses.js
+++ client/components/AddExpenses.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { useOtherExpenses }from "../contexts/OtherExpContext";
 import FoodCard from "./OtherExpCards/Food";
+import CableCard from "./OtherExpCards/Cable";
 import { useAuth } from "../contexts/AuthContext";
 import { Link } from "react-router-dom";
 import {
@@ -46,15 +47,18 @@ const AddExpense = () => {
   ]
 
   return (
-    <div>
-      <h1>Would you like to add other expenses?</h1>
-      <h2>Everyone is unique and has different feelings on what expenses they are willing to cut back on.</h2>
-      <h3>Below are some common expenses that add up over time and you may be able to reduce</h3>
-      <FoodCard />
-      <Link to="/chart">
+    <>
+    <Box sx={{ ml: 2, mr: 2, display: { xs: "none", md: "flex" }, flexWrap: "wrap" }}>
+      {/* <h3>Would you like to add other expenses?</h3>
+      <h4>Everyone is unique and has different feelings on what expenses they are willing to cut back on.</h4>
+      <h4>Below are some common expenses that add up over time and you may be able to reduce</h4> */}
+      <Box sx={{ ml: 1, mr: 1 }}><FoodCard /></Box>
+      <Box sx={{ ml: 1, mr: 1 }}><CableCard /></Box>
+    </Box>
+    <Link to="/chart">
         <button>continue</button>
       </Link>
-    </div>
+    </>
   );
 };
 
