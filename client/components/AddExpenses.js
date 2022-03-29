8e41ac80184c33b054fe4131587d0e980b08e4b1
Rob Watkins <rwatkins123@gmail.com>
2022-03-29 11:15:08 -0500

    food card

--- client/components/AddExpenses.js
+++ client/components/AddExpenses.js
@@ -45,17 +45,38 @@ const AddExpense = () => {
   return (
     <div>
       <h1>Would you like to add other potentially unnessasary expenses?</h1>
-        <h2>Think about what you spent last month that you could have avoided or could be reduced in future months</h2>
-        <h3>Feel free to take a guess or take a look at your recent account statements</h3>
-        <h3>Now take a look below at the list below and check any that you'd like to add an expense to</h3>
-      <TextField
-  //       InputProps={{
-  //         inputComponent: MyInputComponent,
-  //         inputProps: {
-  //           component: SomeThirdPartyComponent,
-  //   },
-  // }}
-/>;
+      <h2>Think about what you spent last month that you could have avoided or could be reduced in future months</h2>
+      <h3>Feel free to take a guess or take a look at your recent account statements</h3>
+      <h3>Now take a look below at the list below and check any that you'd like to add an expense to</h3>
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="Food Delivery"
+        height="140"
+        image="https://technofaq.org/wp-content/uploads/2019/10/whereyatcom_521663620.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Food Delivery/Pick-Up
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Check your order history of any food delivery services you use (uberEats, Doordash, Grubhub, etc.) or look over recent account statements.
+          Now fill in how much $ you think you could cut back on this expense monthly.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <TextField
+        id="filled-number"
+        label="Monthly $ Cutback"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="filled"
+        />
+        <Button size="small">Save</Button>
+      </CardActions>
+    </Card>
       <Link to="/chart">
         <button>continue</button>
       </Link>
