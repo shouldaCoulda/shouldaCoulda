7a6a172dffc7ed608066d524d0400b44d67fca24
joshua constine <joshua.constine97@gmail.com>
2022-03-29 22:49:34 -0500

    updated expense card file struct

--- client/components/AddExpenses.js
+++ client/components/AddExpenses.js
@@ -1,13 +1,8 @@
 import React, { useRef } from "react";
 import { useOtherExpenses } from "../contexts/OtherExpContext";
-import FoodCard from "./OtherExpCards/Food";
-import CableCard from "./OtherExpCards/Cable";
-import TobaccoCard from "./OtherExpCards/Tobacco";
-import CoffeeCard from "./OtherExpCards/Coffee";
-import MiscCard from "./OtherExpCards/Misc";
+
 import ExpenseCard from "./OtherExpCards/ExpenseCard";
 import { useAuth } from "../contexts/AuthContext";
-import { Link } from "react-router-dom";
 import {
   Container,
   TextField,
@@ -21,8 +16,10 @@ import {
   Input,
   InputAdornment,
 } from "@mui/material";
+import { Link, useHistory } from "react-router-dom";
 
 const AddExpense = () => {
+  const history = useHistory();
   const { writeOtherExpenseData } = useOtherExpenses();
   const { writeUserData, currentUser } = useAuth();
   let nameRef = useRef("");
@@ -48,20 +45,33 @@ const AddExpense = () => {
 
   return (
     <>
-      <div>Would you like to add other expenses?</div>
       <Box
         sx={{
           ml: 2,
           mr: 2,
           display: { xs: "none", md: "flex" },
           flexWrap: "wrap",
+          paddingBottom: 20,
+          alignItems: "center",
         }}
       >
+        <div>Would you like to add other expenses?</div>
         <ExpenseCard />
+        <Button
+          onClick={() => history.push("/")}
+          sx={{
+            marginTop: 5,
+            borderWidth: 0,
+            boxShadow: "3px 2px 10px darkgray",
+            borderCollapse: "collapse",
+            color: "black",
+            borderRadius: 40,
+            width: 90,
+          }}
+        >
+          Next
+        </Button>
       </Box>
-      <Link to="/chart">
-        <button>continue</button>
-      </Link>
     </>
   );
 };
--- client/components/OtherExpCards/ExpenseCard.js
+++ client/components/OtherExpCards/ExpenseCard.js
@@ -15,58 +15,67 @@ import {
 import { expenseCards } from "../../../script/OtherExpenses";
 
 export default function ExpenseCard() {
-  console.log(expenseCards);
   return (
     <Box
       sx={{
-        mr: 2,
         display: { xs: "none", md: "flex" },
         flexDirection: "column",
-        alignItems: "center",
-        flexWrap: "wrap",
-        padding: 2,
         paddingBottom: 20,
+        alignItems: "center",
       }}
     >
-      {expenseCards.map((expense, index) => {
-        return (
-          <Box key={index}>
-            <Card key={index} sx={{ maxWidth: 200 }}>
-              <CardMedia
-                component="img"
-                alt="Misc"
-                height="140"
-                image={expense.imageUrl}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {expense.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {expense.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <TextField
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">$</InputAdornment>
-                    ),
-                  }}
-                  id="filled-number"
-                  label="Monthly $ Cutback"
-                  type="number"
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  variant="outlined"
+      <Box
+        sx={{
+          mr: 2,
+          display: { xs: "none", md: "flex" },
+          flexDirection: "row",
+          alignItems: "center",
+          flexWrap: "wrap",
+          padding: 2,
+          paddingBottom: 20,
+        }}
+      >
+        {expenseCards.map((expense, index) => {
+          return (
+            <Box key={index}>
+              <Card key={index} sx={{ width: 200, height: 650, margin: 4 }}>
+                <CardMedia
+                  component="img"
+                  alt="Misc"
+                  height="140"
+                  image={expense.imageUrl}
                 />
-                <Button size="small">Save</Button>
-              </CardActions>
-            </Card>
-          </Box>
-        );
-      })}
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {expense.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {expense.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <TextField
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">$</InputAdornment>
+                      ),
+                    }}
+                    id="filled-number"
+                    label="Monthly $ Cutback"
+                    type="number"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    variant="outlined"
+                  />
+                  <Button size="small">Save</Button>
+                </CardActions>
+              </Card>
+            </Box>
+          );
+        })}
+      </Box>
+      
     </Box>
   );
 }
