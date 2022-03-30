0f990e1f9626eaff171e7584130b462d456403cb
Rob Watkins <rwatkins123@gmail.com>
2022-03-30 11:06:03 -0500

    expenses being targeted

--- client/components/OtherExpCards/ExpenseCard.js
+++ client/components/OtherExpCards/ExpenseCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Container,
   TextField,
@@ -11,10 +11,20 @@ import {
   CardMedia,
   Input,
   InputAdornment,
+  Collapse,
+  IconButton,
+  FormControl,
+  InputLabel,
+  FormHelperText,
 } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { expenseCards } from "../../../script/OtherExpenses";
 
+
 export default function ExpenseCard() {
+
+
   return (
     <Box
       sx={{
@@ -27,9 +37,36 @@ export default function ExpenseCard() {
       }}
     >
       {expenseCards.map((expense, index) => {
+
+        const handleSubmit = async (e) => {
+          console.log("submit", expense.name, "price", priceRef.current.value);
+          // e.preventDefault();
+          // writeSubscriptionData(
+          //   nameRef.current.value,
+          //   priceRef.current.value,
+          //   currentUser.uid
+          // );
+        };
+
+        const [expanded, setExpanded] = React.useState(false);
+        const priceRef = useRef()
+        const handleExpandClick = () => {
+          setExpanded(!expanded);
+        };
+        const ExpandMore = styled((props) => {
+          const { expand, ...other } = props;
+          return <IconButton {...other} />;
+        })(({ theme, expand }) => ({
+          transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+          marginLeft: 'auto',
+          transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+          }),
+        }));
+
         return (
           <Box key={index}>
-            <Card key={index} sx={{ width: 250, height: 450, margin: 1 }}>
+            <Card key={index} sx={{ width: 250, margin: 1 }}>
               <CardMedia
                 component="img"
                 alt="Misc"
@@ -44,6 +81,26 @@ export default function ExpenseCard() {
                   {expense.description}
                 </Typography>
               </CardContent>
+                <CardActions>
+                <ExpandMore
+                  expand={expanded}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+       <FormControl>
+        <InputLabel htmlFor="Price">Price</InputLabel>
+        <Input aria-describedby="my-helper-text" inputRef={priceRef} />
+        <FormHelperText>monthly cost:</FormHelperText>
+      </FormControl>
+      <Button onClick={handleSubmit}>submit</Button>
+        </CardContent>
+      </Collapse>
             </Card>
           </Box>
         );
