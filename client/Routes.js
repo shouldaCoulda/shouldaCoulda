84a105df6b08aae990a9476d89e6e012bfd4f638
Chao Zhou <Czhou0k@gmail.com>
2022-03-30 23:14:14 -0400

    added a bar chart route for testing

--- client/Routes.js
+++ client/Routes.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { connect } from "react-redux";
-import { useAuth } from "./contexts/AuthContext";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import Profile from "./components/Profile";
-import ChartContainer from "./components/Chart/ChartContainer";
-import AddSubscription from "./components/AddSubscription";
-import SelectionScreen from "./components/SelectionScreen";
-import { SubscriptionTier } from "./components/SubscriptionTier";
-import SignInPrompt from "./components/SignInPrompt";
-import Home from "./components/Home";
-import AddExpensees from "./components/AddExpenses";
-import LandingPage from "./components/LandingPage";
+import React from 'react';
+import { connect } from 'react-redux';
+import { useAuth } from './contexts/AuthContext';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Profile from './components/Profile';
+import ChartContainer from './components/Chart/ChartContainer';
+import AddSubscription from './components/AddSubscription';
+import SelectionScreen from './components/SelectionScreen';
+import { SubscriptionTier } from './components/SubscriptionTier';
+import SignInPrompt from './components/SignInPrompt';
+import Home from './components/Home';
+import AddExpensees from './components/AddExpenses';
+import LandingPage from './components/LandingPage';
+import BarChart from './components/BarChart';
 
 const Routes = () => {
   const { currentUser } = useAuth();
@@ -22,23 +23,24 @@ const Routes = () => {
       <Switch>
         {currentUser ? (
           <>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/chart" component={ChartContainer} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/selections" component={SelectionScreen} />
-            <Route exact path="/add" component={AddSubscription} />
-            <Route exact path="/expense" component={AddExpensees} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/chart' component={ChartContainer} />
+            <Route exact path='/profile' component={Profile} />
+            <Route exact path='/selections' component={SelectionScreen} />
+            <Route exact path='/add' component={AddSubscription} />
+            <Route exact path='/expense' component={AddExpensees} />
             <Route
               exact
-              path="/subscriptioninfo"
+              path='/subscriptioninfo'
               component={SubscriptionTier}
             />
           </>
         ) : (
           <>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route path="/" component={LandingPage} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+            <Route path='/' component={LandingPage} />
+            <Route path='/test' component={BarChart} />
           </>
         )}
       </Switch>
