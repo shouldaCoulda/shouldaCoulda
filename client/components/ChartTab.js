c298bb8276045a7e18faa1f7c38e4c1891d34530
Chao Zhou <Czhou0k@gmail.com>
2022-03-31 11:50:25 -0400

    added a ChartTab component to reuse

---
+++ client/components/ChartTab.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+const ChartTab = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant='scrollable'
+        scrollButtons='auto'
+        aria-label='scrollable auto tabs example'
+      >
+        <Tab label='Item One' />
+        <Tab label='Item Two' />
+        <Tab label='Item Three' />
+        <Tab label='Item Four' />
+        <Tab label='Item Five' />
+        <Tab label='Item Six' />
+        <Tab label='Item Seven' />
+      </Tabs>
+    </Box>
+  );
+};
+export default ChartTab;
