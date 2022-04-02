cbc50d66103e6c82ac5bda4e480e3be474de9408
Chao Zhou <Czhou0k@gmail.com>
2022-04-01 20:56:01 -0400

    added a bar chart component

--- client/Routes.js
+++ client/Routes.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { connect } from "react-redux";
-import { useAuth } from "./contexts/AuthContext";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import Profile from "./components/Profile";
-import ChartContainer from "./components/Chart/ChartContainer";
-import AddSubscription from "./components/AddSubscription";
-import SelectionScreen from "./components/SelectionScreen";
-import { SubscriptionTier } from "./components/SubscriptionTier";
-import Home from "./components/Home";
-import AddExpensees from "./components/AddExpenses";
-import LandingPage from "./components/LandingPage";
-import PieChart from "./components/PieChart";
-import ListChart from "./components/ListChart";
-import About from "./components/Footer/About";
-import Team from "./components/Footer/Team";
+import React from 'react';
+import { connect } from 'react-redux';
+import { useAuth } from './contexts/AuthContext';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Profile from './components/Profile';
+import ChartContainer from './components/Chart/ChartContainer';
+import AddSubscription from './components/AddSubscription';
+import SelectionScreen from './components/SelectionScreen';
+import { SubscriptionTier } from './components/SubscriptionTier';
+import Home from './components/Home';
+import AddExpensees from './components/AddExpenses';
+import LandingPage from './components/LandingPage';
+import PieChart from './components/PieChart';
+import ListChart from './components/ListChart';
+import About from './components/Footer/About';
+import Team from './components/Footer/Team';
+import BarChart from './components/BarChart';
 
 const Routes = () => {
   const { currentUser } = useAuth();
@@ -25,30 +26,31 @@ const Routes = () => {
       <Switch>
         {currentUser ? (
           <>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/chart" component={ChartContainer} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/selections" component={SelectionScreen} />
-            <Route exact path="/add" component={AddSubscription} />
-            <Route exact path="/expense" component={AddExpensees} />
-            <Route exact path="/landing" component={LandingPage} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/team" component={Team} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/chart' component={ChartContainer} />
+            <Route exact path='/profile' component={Profile} />
+            <Route exact path='/selections' component={SelectionScreen} />
+            <Route exact path='/add' component={AddSubscription} />
+            <Route exact path='/expense' component={AddExpensees} />
+            <Route exact path='/landing' component={LandingPage} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/team' component={Team} />
             <Route
               exact
-              path="/subscriptioninfo"
+              path='/subscriptioninfo'
               component={SubscriptionTier}
             />
-            <Route exact path="/PieChart" component={PieChart} />
-            <Route exact path="/List" component={ListChart} />
+            <Route exact path='/PieChart' component={PieChart} />
+            <Route exact path='/List' component={ListChart} />
+            <Route exact path='/Bar' component={BarChart} />
           </>
         ) : (
           <>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/team" component={Team} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/" component={SignUp} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/team' component={Team} />
+            <Route exact path='/signup' component={SignUp} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/' component={SignUp} />
           </>
         )}
       </Switch>
---
+++ client/components/BarChart.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { csv, scaleBand, scaleLinear, max } from 'd3';
+
+const BarChart = () => {
+  return <div>TESTING</div>;
+};
+
+export default BarChart;
