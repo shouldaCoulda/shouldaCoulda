d213b85506812c95217677cb20c28edb6ec69325
Chao Zhou <Czhou0k@gmail.com>
2022-04-01 21:06:19 -0400

    got a working bar chart from d3 template

--- client/components/BarChart.js
+++ client/components/BarChart.js
@@ -1,8 +1,53 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { csv, scaleBand, scaleLinear, max } from 'd3';
 
+//this is the data or csv file we are getting the data from
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
+
+const width = 960;
+const height = 500;
+
+//use state in the component to set data
 const BarChart = () => {
-  return <div>TESTING</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const row = (d) => {
+      d.Population = +d['2020'];
+      return d;
+    };
+    csv(csvUrl, row).then((data) => {
+      setData(data.slice(0, 10));
+    });
+  }, []);
+
+  if (!data) {
+    return <pre>Loading...</pre>;
+  }
+
+  console.log(data[0]);
+
+  const yScale = scaleBand()
+    .domain(data.map((d) => d.Country))
+    .range([0, height]);
+
+  const xScale = scaleLinear()
+    .domain([0, max(data, (d) => d.Population)])
+    .range([0, width]);
+
+  return (
+    <svg width={width} height={height}>
+      {data.map((d) => (
+        <rect
+          key={d.Country}
+          y={yScale(d.Country)}
+          width={xScale(d.Population)}
+          height={yScale.bandwidth()}
+        />
+      ))}
+    </svg>
+  );
 };
 
 export default BarChart;
