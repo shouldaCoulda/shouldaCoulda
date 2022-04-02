c70bfed4d6333d41678545011c4801efb82b234b
Chao Zhou <Czhou0k@gmail.com>
2022-04-02 08:43:38 -0400

    made an AxisBottom component and moved code into it

--- client/components/BarChart.js
+++ client/components/BarChart.js
@@ -30,19 +30,6 @@ const useData = () => {
   }, []);
   return data;
 };
-// Function for the Bottom Axis
-const AxisBottom = (xScale, innerHeight) => {
-  xScale.ticks().map((tickValue, i) => {
-    return (
-      <g key={i} transform={`translate(${xScale(tickValue)},0)`}>
-        <line x1={0} y1={0} x2={0} y2={innerHeight} stroke='green' />
-        <text y={innerHeight} style={{ textAnchor: 'middle' }} dy='1.1em'>
-          {tickValue}
-        </text>
-      </g>
-    );
-  });
-};
 
 //use state in the component to set data
 const BarChart = () => {
---
+++ client/components/BarChartRefactor/AxisBottom.js
@@ -0,0 +1,14 @@
+// Function for the Bottom Axis
+//if we use refractor syntax make sure to pass xScale and innerHeight as props in the render BarChart Component when returning
+const AxisBottom = (xScale, innerHeight) => {
+  xScale.ticks().map((tickValue, i) => {
+    return (
+      <g key={i} transform={`translate(${xScale(tickValue)},0)`}>
+        <line x1={0} y1={0} x2={0} y2={innerHeight} stroke='green' />
+        <text y={innerHeight} style={{ textAnchor: 'middle' }} dy='1.1em'>
+          {tickValue}
+        </text>
+      </g>
+    );
+  });
+};
