bf9edb82aa5fe2c29f6affeadfe4232ac2ff5f7e
Chao Zhou <Czhou0k@gmail.com>
2022-04-01 21:09:33 -0400

    added some comments

--- client/components/BarChart.js
+++ client/components/BarChart.js
@@ -12,31 +12,38 @@ const height = 500;
 const BarChart = () => {
   const [data, setData] = useState(null);
 
+  //use effect to retrieve the data
   useEffect(() => {
+    //setting the row data to year 2020 and Population
     const row = (d) => {
       d.Population = +d['2020'];
       return d;
     };
+    //slicing only the first 10 of the data from the list
     csv(csvUrl, row).then((data) => {
       setData(data.slice(0, 10));
     });
   }, []);
 
+  //condition is the data is not exist then give a loading message
   if (!data) {
     return <pre>Loading...</pre>;
   }
 
   console.log(data[0]);
 
+  //set the Y axis mapping the country data and range from 0 to the height
   const yScale = scaleBand()
     .domain(data.map((d) => d.Country))
     .range([0, height]);
 
+  //set the X axis Population from 0 to width
   const xScale = scaleLinear()
     .domain([0, max(data, (d) => d.Population)])
     .range([0, width]);
 
   return (
+    //It use svg tab to show the chart mapping the array and displaying the data in this format
     <svg width={width} height={height}>
       {data.map((d) => (
         <rect
