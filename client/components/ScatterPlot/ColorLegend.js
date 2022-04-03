ad5692e843dc3df1bc54b2d6eaa5303c163f8b95
Chao Zhou <Czhou0k@gmail.com>
2022-04-03 13:33:49 -0400

    fixed the text position of the legend

--- client/components/ScatterPlot/ColorLegend.js
+++ client/components/ScatterPlot/ColorLegend.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 //domainValue is the name of each type
 //this control color legend we see on the page to the right.
-const ColorLegend = ({ colorScale, tickSpacing = 20, tickSize = 10 }) => {
+const ColorLegend = ({
+  colorScale,
+  tickSpacing = 20,
+  tickSize = 10,
+  tickTextOffset = 12,
+}) => {
   return colorScale.domain().map((domainValue, i) => {
     return (
       <g key={i} transform={`translate(0,${i * tickSpacing})`}>
         <circle fill={colorScale(domainValue)} r={tickSize} />
-        <text dy='.32em'>{domainValue}</text>
+        <text x={tickTextOffset} dy='.32em'>
+          {domainValue}
+        </text>
       </g>
     );
   });
