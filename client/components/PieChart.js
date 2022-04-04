46260bb4262943f46ba218b2239fd8011c984658
Chao Zhou <Czhou0k@gmail.com>
2022-04-04 12:43:34 -0400

    have a responsive pie chart

--- client/components/PieChart.js
+++ client/components/PieChart.js
@@ -6,9 +6,18 @@ import { Container } from '@mui/material';
 import { useAuth } from '../contexts/AuthContext';
 
 const PieChart = () => {
-  const { usersSubscriptions, usersExpenses, usersIncomes } = useAuth();
+  const {
+    usersSubscriptions,
+    usersExpenses,
+    usersIncomes,
+    usersTotalIncomeAndExpenses,
+  } = useAuth();
   console.log('this is userSubscriptions', usersSubscriptions);
   console.log('this is userExpenses', usersExpenses);
+  console.log(
+    'this is userTotalIncomeAndExpenses',
+    usersTotalIncomeAndExpenses
+  );
 
   return (
     <div>
@@ -80,12 +89,14 @@ const PieChart = () => {
                 return expense.price;
               }),
               backgroundColor: [
+                'rgba(115, 30, 102, 0.2)',
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)',
+                'rgba(115, 30, 102, 0.2)',
               ],
               borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -94,6 +105,7 @@ const PieChart = () => {
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)',
+                'rgba(115, 30, 102, 1)',
               ],
               borderWidth: 1,
             },
