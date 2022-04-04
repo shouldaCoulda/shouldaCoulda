cc1d66fd55e509a8626b653c42194ea962abd1cf
Chao Zhou <Czhou0k@gmail.com>
2022-04-03 23:20:04 -0400

    added a new bar chart

---
+++ client/components/MultiBarChart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const MultiBarChart = () => {
+  const data = {
+    labels: ['2016', '2017', '2018', '2019', '2020', '2021', '2022'],
+    dataSets: [
+      {
+        label: 'expense',
+        data: [100, 125, 150, 175, 200, 250, 300],
+      },
+    ],
+  };
+  return <Bar data={data} />;
+};
+
+export default MultiBarChart;
