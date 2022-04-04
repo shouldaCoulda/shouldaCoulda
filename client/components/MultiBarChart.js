ce2aaff2fdbaa5c43ebdbb1b5175301ad0ec5a2d
Chao Zhou <Czhou0k@gmail.com>
2022-04-03 23:51:20 -0400

    fixing up some position on chart

--- client/components/MultiBarChart.js
+++ client/components/MultiBarChart.js
@@ -8,21 +8,34 @@ const MultiBarChart = () => {
       {
         label: 'expenses',
         data: [100, 125, 150, 175, 200, 250, 300],
-        backgroundColor: ['rgba(0,10,220,0)'],
+        backgroundColor: ['rgba(0,10,220,1)'],
       },
       {
         label: 'subscriptions',
         data: [200, 100, 170, 50, 99, 300, 400],
-        backgroundColor: ['rgba(255, 165, 0,0)'],
+        backgroundColor: ['rgba(255, 165, 0,1)'],
       },
       {
         label: 'others',
         data: [50, 75, 40, 30, 150, 125, 150],
-        backgroundColor: ['rgb(106, 90, 205,0)'],
+        backgroundColor: ['rgb(106, 90, 205,1)'],
       },
     ],
   };
-  return <Bar data={data} />;
+  const options = {
+    title: {
+      display: true,
+      text: 'Bar Chart',
+    },
+    scales: {
+      yAxis: {
+        ticks: {
+          min: 0,
+        },
+      },
+    },
+  };
+  return <Bar data={data} options={options} />;
 };
 
 export default MultiBarChart;
