c5282bb70cc8dce51cd2e3dee122812c8555bfca
Chao Zhou <Czhou0k@gmail.com>
2022-04-03 23:38:53 -0400

    changed color opacity

--- client/components/MultiBarChart.js
+++ client/components/MultiBarChart.js
@@ -8,17 +8,17 @@ const MultiBarChart = () => {
       {
         label: 'expenses',
         data: [100, 125, 150, 175, 200, 250, 300],
-        backgroundColor: ['rgba(0,10,220,0.8)'],
+        backgroundColor: ['rgba(0,10,220,0)'],
       },
       {
         label: 'subscriptions',
         data: [200, 100, 170, 50, 99, 300, 400],
-        backgroundColor: ['rgba(255, 165, 0,0.8)'],
+        backgroundColor: ['rgba(255, 165, 0,0)'],
       },
       {
         label: 'others',
         data: [50, 75, 40, 30, 150, 125, 150],
-        backgroundColor: ['rgb(106, 90, 205,0.8)'],
+        backgroundColor: ['rgb(106, 90, 205,0)'],
       },
     ],
   };
