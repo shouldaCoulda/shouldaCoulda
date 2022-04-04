dd2fd6c0e240a3a0db03c8f7e21c2c01fd473fda
Chao Zhou <Czhou0k@gmail.com>
2022-04-03 22:12:41 -0400

    added a bit more to stack chart

--- client/components/StackedChart/StackedChart.js
+++ client/components/StackedChart/StackedChart.js
@@ -9,51 +9,59 @@ const data = [
     subscription: 10,
     other: 20,
     expense: 30,
+    cash: 50,
   },
   {
     year: 1990,
     subscription: 20,
     other: 40,
     expense: 60,
+    cash: 70,
   },
   {
     year: 2000,
     subscription: 30,
     other: 45,
     expense: 80,
+    cash: 90,
   },
   {
     year: 2010,
     subscription: 40,
     other: 60,
     expense: 100,
+    cash: 120,
   },
   {
     year: 2020,
     subscription: 50,
     other: 80,
     expense: 120,
+    cash: 150,
   },
   {
     year: 2025,
     subscription: 60,
     other: 90,
     expense: 130,
+    cash: 170,
   },
   {
     year: 2030,
     subscription: 100,
     other: 120,
     expense: 200,
+    cash: 250,
   },
 ];
 
-const allKeys = ['subscription', 'other', 'expense'];
+const allKeys = ['subscription', 'other', 'expense', 'cash'];
 
 const colors = {
-  subscription: 'green',
+  subscription: 'blue',
   other: 'orange',
   expense: 'purple',
+  cash: 'green',
 };
 
 function App() {
