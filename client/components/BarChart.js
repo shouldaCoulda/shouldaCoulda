b5d430cd58f11f874ab66f94f0918b7144343220
Chao Zhou <Czhou0k@gmail.com>
2022-04-04 17:50:41 -0400

    deleted some unused code

--- client/components/BarChart.js
+++ client/components/BarChart.js
@@ -6,41 +6,6 @@ import ChartTab from './ChartTab';
 import { Chart } from 'react-chartjs-2';
 import { useAuth } from '../contexts/AuthContext';
 
-//this is the data or csv file we are getting the data from
-// const csvUrl =
-//   'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
-
-//custom hook made for retrieving data
-// const useData = () => {
-//   const [data, setData] = useState([]);
-
-//   //use effect to retrieve the data
-//   useEffect(() => {
-//     //setting the row data to year 2020 and Population
-//     const row = (d) => {
-//       //+d is setting the data to a number for year 2020
-//       d.Population = +d['2020'];
-//       return d;
-//     };
-//     //slicing only the first 10 of the data from the list
-//     csv(csvUrl, row).then((data) => {
-//       //setting data to data array
-//       setData(data.slice(0, 10));
-//     });
-//   }, []);
-//   return data;
-// };
-
-//dummy data
-const chart = [
-  { name: 'Netflix', price: 9.99 },
-  { name: 'Hulu', price: 12.99 },
-  { name: 'Spotify', price: 15.99 },
-  { name: 'Amazon', price: 20 },
-  { name: 'Youtube', price: 17 },
-  { name: 'HBO Max', price: 9.99 },
-];
-
 const width = 960;
 const height = 500;
 const margin = { top: 35, right: 20, bottom: 70, left: 200 };
@@ -151,60 +116,6 @@ const BarChart = () => {
           {data.map((d, i) => (
             <rect
               className={styles.marks}
-              key={i}
-              y={yScale(yValue(d))}
-              width={xScale(xValue(d))}
-              height={yScale.bandwidth()}
-            >
-              <title>{xValue(d)}</title>
-            </rect>
-          ))}
-        </g>
-      </svg>
-      <svg width={width} height={height}>
-        <g
-          className={styles.tick}
-          transform={`translate(${margin.left},${margin.top})`}
-        >
-          {xScale.ticks().map((tickValue, i) => {
-            return (
-              <g key={i} transform={`translate(${xScale(tickValue)},0)`}>
-                <line x1={0} y1={0} x2={0} y2={innerHeight} />
-                <text
-                  y={innerHeight}
-                  style={{ textAnchor: 'middle' }}
-                  dy='1.1em'
-                >
-                  {tickValue}
-                </text>
-              </g>
-            );
-          })}
-          {yScale.domain().map((tickValue, i) => {
-            return (
-              <g
-                key={i}
-                transform={`translate(0,${
-                  yScale(tickValue) + yScale.bandwidth() / 2
-                })`}
-              >
-                <text style={{ textAnchor: 'end' }} x={-4} dy='.32em'>
-                  {tickValue}
-                </text>
-              </g>
-            );
-          })}
-          <text
-            className={styles.textAxis}
-            x={innerWidth / 2}
-            y={-5}
-            textAnchor='middle'
-          >
-            Expenses
-          </text>
-          {data.map((d, i) => (
-            <rect
-              className={styles.marks_two}
               key={i}
               y={yScale(yValue(d))}
               width={xScale(xValue(d))}
