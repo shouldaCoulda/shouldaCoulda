0919ca41c3716bb164ab1e2934066afc95ddf733
Chao Zhou <Czhou0k@gmail.com>
2022-04-04 21:41:47 -0400

    got a better working side bar chart

--- client/components/MultiBarChart.js
+++ client/components/MultiBarChart.js
@@ -8,53 +8,55 @@ const MultiBarChart = () => {
     usersSubscriptions,
     usersExpenses,
     usersIncomes,
-    usersTotalIncomeAndExpenses,
+    getTotalSubscriptions,
   } = useAuth();
+  console.log(getTotalSubscriptions());
   const data = {
     labels: [
-      ...usersSubscriptions.map((sub) => {
+      usersSubscriptions.map((sub, i) => {
         return sub.name;
       }),
     ],
     datasets: [
       {
-        label: ['Subscription'],
-        data: [
-          ...usersSubscriptions.map((sub) => {
-            return sub.price;
-          }),
+        axis: 'y',
+        label: 'Subscriptions',
+        data: [getTotalSubscriptions()],
+
+        fill: false,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)',
         ],
-        backgroundColor: ['rgba(0,10,220,1)', 'rgba(215, 169, 201, 1)'],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)',
         ],
+        borderWidth: 1,
       },
     ],
   };
   const options = {
-    title: {
-      display: true,
-      text: 'Bar Chart',
-    },
+    indexAxis: 'y',
     scales: {
-      yAxis: {
+      y: {
         ticks: {
-          min: 0,
+          crossAlign: 'far',
         },
       },
     },
   };
-  return (
-    <>
-      <ChartTab />
-      <Bar data={data} options={options} />
-    </>
-  );
+  return <Bar data={data} options={options} />;
 };
 
 export default MultiBarChart;
